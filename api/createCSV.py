39e052ccaa60133e27f4a41bb24af6df9764820c
johncheong1234 <john.cheong.2019@smu.edu.sg>
2023-03-23 19:26:04 +0800

    turning function into string

--- api/createCSV.py
+++ api/createCSV.py
@@ -2,24 +2,24 @@ import opcode
 import sys
 import csv
 
-def isMatch(s: str, p: str) -> bool:
-        s, p = ' '+ s, ' '+ p
-        lenS, lenP = len(s), len(p)
-        dp = [[0]*(lenP) for i in range(lenS)]
-        dp[0][0] = 1
+# def isMatch(s: str, p: str) -> bool:
+#         s, p = ' '+ s, ' '+ p
+#         lenS, lenP = len(s), len(p)
+#         dp = [[0]*(lenP) for i in range(lenS)]
+#         dp[0][0] = 1
 
-        for j in range(1, lenP):
-            if p[j] == '*':
-                dp[0][j] = dp[0][j-2]
+#         for j in range(1, lenP):
+#             if p[j] == '*':
+#                 dp[0][j] = dp[0][j-2]
 
-        for i in range(1, lenS):
-            for j in range(1, lenP):
-                if p[j] in {s[i], '.'}:
-                    dp[i][j] = dp[i-1][j-1]
-                elif p[j] == "*":
-                    dp[i][j] = dp[i][j-2] or int(dp[i-1][j] and p[j-1] in {s[i], '.'})
+#         for i in range(1, lenS):
+#             for j in range(1, lenP):
+#                 if p[j] in {s[i], '.'}:
+#                     dp[i][j] = dp[i-1][j-1]
+#                 elif p[j] == "*":
+#                     dp[i][j] = dp[i][j-2] or int(dp[i-1][j] and p[j-1] in {s[i], '.'})
 
-        return bool(dp[-1][-1])
+#         return bool(dp[-1][-1])
 
 def show_trace(frame, event, arg):
     frame.f_trace_opcodes = True
