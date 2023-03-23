b80d4d04ac404f5a606f3e69f220dd5f4ae97163
johncheong1234 <john.cheong.2019@smu.edu.sg>
2023-03-23 12:47:26 +0800

    import function and write to csv

--- watcherTest.py
+++ watcherTest.py
@@ -1,5 +1,7 @@
 import opcode  
 import sys
+import csv
+from functions import *
 
 # def fib(n):
 #     i, f1, f2 = 1, 1, 1
@@ -7,25 +9,6 @@ import sys
 #         f1, f2 = f2, f1 + f2
 #         i += 1
 #     return f1
-
-def isMatch(s: str, p: str) -> bool:
-        s, p = ' '+ s, ' '+ p
-        lenS, lenP = len(s), len(p)
-        dp = [[0]*(lenP) for i in range(lenS)]
-        dp[0][0] = 1
-
-        for j in range(1, lenP):
-            if p[j] == '*':
-                dp[0][j] = dp[0][j-2]
-
-        for i in range(1, lenS):
-            for j in range(1, lenP):
-                if p[j] in {s[i], '.'}:
-                    dp[i][j] = dp[i-1][j-1]
-                elif p[j] == "*":
-                    dp[i][j] = dp[i][j-2] or int(dp[i-1][j] and p[j-1] in {s[i], '.'})
-
-        return bool(dp[-1][-1])
 
 def firstMissingPositive(nums):
     # res = 1
@@ -38,23 +21,44 @@ def firstMissingPositive(nums):
             return i+1
     
     return len(nums)+1
-    # res = len(nums)+1
 
+# create recursive function with base case
+def fib(n):
+    if n == 0:
+        return 0
+    elif n == 1:
+        return 1
+    else:
+        return fib(n-1) + fib(n-2)
+
+# create a list called algoData
+algoData = []
 
 def show_trace(frame, event, arg):
     frame.f_trace_opcodes = True
     code = frame.f_code
     offset = frame.f_lasti
     
-    if event == 'line':
-        print(f"| {event:10} | {str(arg):>4} |", end=' ')
-        print(f"{frame.f_lineno:>4} | {frame.f_lasti:>6} |", end=' ')
-        print(f"{opcode.opname[code.co_code[offset]]:<18} | {str(frame.f_locals):<35} |")
+    print(f"| {event:10} | {str(arg):>4} |", end=' ')
+    print(f"{frame.f_lineno:>4} | {frame.f_lasti:>6} |", end=' ')
+    print(f"{opcode.opname[code.co_code[offset]]:<18} | {str(frame.f_locals):<35} |")
+
+
+    with open ('algoData.csv', 'a', newline='') as f:
+        writer = csv.writer(f)
+        writer.writerow([event, arg, frame.f_lineno, frame.f_lasti, opcode.opname[code.co_code[offset]], str(frame.f_locals)])
+
     return show_trace
 
 header = f"| {'event':10} | {'arg':>4} | line | offset | {'opcode':^18} | {'locals':^35} |"
+
+with open ('algoData.csv', 'w', newline='') as f:
+    writer = csv.writer(f)
+    writer.writerow(['event', 'arg', 'line', 'offset', 'opcode', 'locals'])
+
+
 print(header)
 sys.settrace(show_trace)
-# isMatch("aa", "a*")
-firstMissingPositive([0,1,2])
+isMatch("aa", "a*")
 sys.settrace(None)
+
