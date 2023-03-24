ba2a892cf9c575cc3baf24cbb52904ce68e4366a
johncheong1234 <john.cheong.2019@smu.edu.sg>
2023-03-24 09:58:41 +0800

    install cors

--- api/hello.py
+++ api/hello.py
@@ -1,6 +1,9 @@
 from flask import Flask, request, jsonify
 from createCSV import createCSV
+from flask_cors import CORS
+
 app = Flask(__name__)
+CORS(app)
 
 @app.route('/')
 def hello_world():
@@ -11,8 +14,8 @@ def submit():
     # process body of request
     if request.method == 'POST':
         data = request.get_json()
-        print(data['test'])
-        
+        print(data)
+
 #         functionCall = s = """
 # def test(j):
 #     n = 10
@@ -23,9 +26,30 @@ def submit():
 # test(1)
 # """
 
+# def isMatch(s: str, p: str) -> bool:
+#         s, p = ' '+ s, ' '+ p
+#         lenS, lenP = len(s), len(p)
+#         dp = [[0]*(lenP) for i in range(lenS)]
+#         dp[0][0] = 1
+
+#         for j in range(1, lenP):
+#             if p[j] == '*':
+#                 dp[0][j] = dp[0][j-2]
+
+#         for i in range(1, lenS):
+#             for j in range(1, lenP):
+#                 if p[j] in {s[i], '.'}:
+#                     dp[i][j] = dp[i-1][j-1]
+#                 elif p[j] == "*":
+#                     dp[i][j] = dp[i][j-2] or int(dp[i-1][j] and p[j-1] in {s[i], '.'})
+
+#         return bool(dp[-1][-1])
+
         # create a string with multiple lines
-        functionCall = 'def test(j):\n    n = 10\n    for i in range(1, n):\n        j = i + j\n    return j\n\ntest(4)\n'
-        print(functionCall)
+        # functionCall = 'def test(j):\n    n = 10\n    for i in range(1, n):\n        j = i + j\n    return j\n\ntest(4)\n'
+        # functionCall = data['code']
+        functionCall = 'def isMatch(s: str, p: str) -> bool:\n    s, p = \' \' + s, \' \' + p\n    lenS, lenP = len(s), len(p)\n    dp = [[0]*(lenP) for i in range(lenS)]\n    dp[0][0] = 1\n\n    for j in range(1, lenP):\n        if p[j] == \'*\':\n            dp[0][j] = dp[0][j-2]\n\n    for i in range(1, lenS):\n        for j in range(1, lenP):\n            if p[j] in {s[i], \'.\'}:\n                dp[i][j] = dp[i-1][j-1]\n            elif p[j] == "*":\n                dp[i][j] = dp[i][j-2] or int(dp[i-1][j] and p[j-1] in {s[i], \'.\'})\n\n    return bool(dp[-1][-1])\n\nisMatch("aa", "a*")\n'
+        print(data['code'])
         createCSV(functionCall)
         return jsonify(data)
 
