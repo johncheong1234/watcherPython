5aa2ef389c134fc24d1f526ecd911a7e267000fc
johncheong1234 <john.cheong.2019@smu.edu.sg>
2023-03-24 16:44:12 +0800

    only return certain local objects + code lines

--- api/createCSV.py
+++ api/createCSV.py
@@ -1,6 +1,8 @@
 import opcode  
 import sys
 import csv
+import uuid
+from typing import List
 
 # def isMatch(s: str, p: str) -> bool:
 #         s, p = ' '+ s, ' '+ p
@@ -21,33 +23,31 @@ import csv
 
 #         return bool(dp[-1][-1])
 
+randomId = uuid.uuid4()
+csvFileName = 'algoData' + str(randomId) + '.csv'
+
 def show_trace(frame, event, arg):
     frame.f_trace_opcodes = True
     code = frame.f_code
     offset = frame.f_lasti
     
-    print(f"| {event:10} | {str(arg):>4} |", end=' ')
-    print(f"{frame.f_lineno:>4} | {frame.f_lasti:>6} |", end=' ')
-    print(f"{opcode.opname[code.co_code[offset]]:<18} | {str(frame.f_locals):<35} |")
+    # print(f"| {event:10} | {str(arg):>4} |", end=' ')
+    # print(f"{frame.f_lineno:>4} | {frame.f_lasti:>6} |", end=' ')
+    # print(f"{opcode.opname[code.co_code[offset]]:<18} | {str(frame.f_locals):<35} |")
+    # loop through frame.f_locals and print them out:
+    localObjects = {}
+    for key, value in frame.f_locals.items():
+        print(f"{key}: {value}")
+        if isinstance(value, (int, float, complex, str, bool, list, tuple, set, dict)):
+            localObjects[key] = value
 
-
-    with open ('algoData.csv', 'a', newline='') as f:
+    with open (csvFileName, 'a', newline='') as f:
         writer = csv.writer(f)
-        writer.writerow([event, arg, frame.f_lineno, frame.f_lasti, opcode.opname[code.co_code[offset]], str(frame.f_locals)])
+        writer.writerow([event, arg, frame.f_lineno, frame.f_lasti, opcode.opname[code.co_code[offset]], frame.f_locals, localObjects])
 
     return show_trace
 
 header = f"| {'event':10} | {'arg':>4} | line | offset | {'opcode':^18} | {'locals':^35} |"
-
-with open ('algoData.csv', 'w', newline='') as f:
-    writer = csv.writer(f)
-    writer.writerow(['event', 'arg', 'line', 'offset', 'opcode', 'locals'])
-
-
-# print(header)
-# sys.settrace(show_trace)
-# isMatch("aa", "a*")
-# sys.settrace(None)
 
 def test(j):
     n = 10
@@ -56,12 +56,49 @@ def test(j):
     return j
 
 def createCSV(functionCallString):
-    with open ('algoData.csv', 'w', newline='') as f:
+    #generate a random ID for the csv file
+    with open (csvFileName, 'w', newline='') as f:
         writer = csv.writer(f)
-        writer.writerow(['event', 'arg', 'line', 'offset', 'opcode', 'locals'])
+        writer.writerow(['event', 'arg', 'line', 'offset', 'opcode', 'locals', 'localObjects'])
 
     sys.settrace(show_trace)
+    # exec(functionCallString)
+#     functionCallString = """
+# class Solution:
+#     def isMatch(self, s: str, p: str) -> bool:
+#         cache = {}
+#         def rec(s_index, p_index):
+#             if (s_index, p_index) in cache: 
+#                 return cache[(s_index, p_index)]
+            
+#             if s_index >= len(s) and p_index >= len(p): 
+#                 cache[(s_index, p_index)] = True
+#                 return cache[(s_index, p_index)]
+            
+#             if p_index >= len(p): 
+#                 cache[(s_index, p_index)] = False
+#                 return cache[(s_index, p_index)]
+            
+#             match = (s_index < len(s) and p[p_index] in {s[s_index], '.'} )
+            
+#             if p_index + 1 <len(p) and p[p_index +1] == '*':                
+#                 cache[s_index, p_index] = rec(s_index, p_index + 2) or (match and rec(s_index + 1, p_index))               
+#                 return cache[s_index, p_index]
+            
+#             if match:
+#                 cache[s_index, p_index] = rec(s_index + 1, p_index+1)
+#                 return cache[s_index, p_index]
+            
+#             cache[s_index, p_index] = False
+#             return cache[s_index, p_index]
+        
+#         return rec(0, 0)
+
+# solution = Solution()
+# solution.isMatch("aa", "a*")
+#     """
     exec(functionCallString)
     sys.settrace(None)
+    return csvFileName
     
 
--- api/hello.py
+++ api/hello.py
@@ -1,6 +1,8 @@
 from flask import Flask, request, jsonify
 from createCSV import createCSV
 from flask_cors import CORS
+import csv
+import os
 
 app = Flask(__name__)
 CORS(app)
@@ -14,46 +16,27 @@ def submit():
     # process body of request
     if request.method == 'POST':
         data = request.get_json()
-        print(data)
+        csvFileName = createCSV(functionCallString=data['code'])
 
-#         functionCall = s = """
-# def test(j):
-#     n = 10
-#     for i in range(1, n):
-#         j = i + j
-#     return j
+        # break code into lines based on line break
+        codeLines = data['code'].splitlines()
+        print('codeLines are ', codeLines)
+        visualList = []
+        with open(csvFileName, 'r') as f:
+            reader = csv.DictReader(f)
+            for row in reader:
+                # check if the line number is valid
+                if 1<int(row['line']) < len(codeLines):
+                    codeLinePrior = codeLines[int(row['line'])-2]
+                    codeLineAt = codeLines[int(row['line'])-1]
+                    row['codeLineAt'] = codeLineAt
+                    row['codeLinePrior'] = codeLinePrior
+                visualList.append(row)
+        
+        # if(len(visualList) >0):
+        #     os.remove(csvFileName)
+        return {'visualList': visualList}
 
-# test(1)
-# """
-
-# def isMatch(s: str, p: str) -> bool:
-#         s, p = ' '+ s, ' '+ p
-#         lenS, lenP = len(s), len(p)
-#         dp = [[0]*(lenP) for i in range(lenS)]
-#         dp[0][0] = 1
-
-#         for j in range(1, lenP):
-#             if p[j] == '*':
-#                 dp[0][j] = dp[0][j-2]
-
-#         for i in range(1, lenS):
-#             for j in range(1, lenP):
-#                 if p[j] in {s[i], '.'}:
-#                     dp[i][j] = dp[i-1][j-1]
-#                 elif p[j] == "*":
-#                     dp[i][j] = dp[i][j-2] or int(dp[i-1][j] and p[j-1] in {s[i], '.'})
-
-#         return bool(dp[-1][-1])
-
-        # create a string with multiple lines
-        # functionCall = 'def test(j):\n    n = 10\n    for i in range(1, n):\n        j = i + j\n    return j\n\ntest(4)\n'
-        # functionCall = data['code']
-        functionCall = 'def isMatch(s: str, p: str) -> bool:\n    s, p = \' \' + s, \' \' + p\n    lenS, lenP = len(s), len(p)\n    dp = [[0]*(lenP) for i in range(lenS)]\n    dp[0][0] = 1\n\n    for j in range(1, lenP):\n        if p[j] == \'*\':\n            dp[0][j] = dp[0][j-2]\n\n    for i in range(1, lenS):\n        for j in range(1, lenP):\n            if p[j] in {s[i], \'.\'}:\n                dp[i][j] = dp[i-1][j-1]\n            elif p[j] == "*":\n                dp[i][j] = dp[i][j-2] or int(dp[i-1][j] and p[j-1] in {s[i], \'.\'})\n\n    return bool(dp[-1][-1])\n\nisMatch("aa", "a*")\n'
-        print(data['code'])
-        createCSV(functionCall)
-        return jsonify(data)
-
-    return 'Submit'
 
 if __name__ == '__main__':
     app.run()
